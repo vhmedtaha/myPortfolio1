3aa01007eefeaaa8656aa959967f9b6bdf0a4b83
vhmedtaha <121405429+vhmedtaha@users.noreply.github.com>
2023-11-23 00:00:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop-150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if(top >=offset && top < offset + height ){
+        navLinks.forEach(links =>{
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        })
+    }
+
+    })
+}
+ScrollReveal({
+    reset: true ,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+})
+
+ScrollReveal().reveal('.home-content, .heading',  { origin: 'top' });
+ScrollReveal().reveal('.home-image, .services-container, .portfolio-box,.contact form',  { origin: 'bottom' });
+
+const typed = new typed('.multi-text',{
+    strings:['Front-End Developer', 'UI UX','Blogger'],
+    typedSpeed:100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true 
+});
